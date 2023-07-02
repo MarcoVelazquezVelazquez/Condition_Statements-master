068063b379de8420acfb8830a24a1b7d76180451
Marco Antonio Velázquez Velázquez <velazquezv.marco@gmail.com>
2023-07-02 11:11:19 -0600

    Solved

--- src/main/java/com/epam/rd/autotasks/ConditionStatements.java
+++ src/main/java/com/epam/rd/autotasks/ConditionStatements.java
@@ -3,10 +3,23 @@ package com.epam.rd.autotasks;
 import java.rmi.UnexpectedException;
 
 class ConditionStatements {
-    public static int task1(int n) {
-        //TODO: Delete line below and write your own solution
-        throw new UnsupportedOperationException();
+
+    public static void main(String[] args) {
+        System.out.println(ConditionStatements.task1(4));
+        System.out.println(ConditionStatements.task1(-5));
+        System.out.println(ConditionStatements.task1(0));
     }
+    public static int task1(int n) {
+        if (n >= 0) {
+            return n * n;
+        }
+        if (n < 0) {
+            return n * -1;
+        }
+        if (n == 0) {
+            return n = 0;
+        }
 
-
+        return n;
+    }
 }
